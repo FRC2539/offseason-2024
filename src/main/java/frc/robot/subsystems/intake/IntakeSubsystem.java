c4cf87ef98c51ccf69c6e81adb931616b71d54b5
Elliptication <amajha@hershey.k12.pa.us>
2024-05-01 20:12:04 -0400

    first commit

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.framework.motor.MotorIO;
+
+public class IntakeSubsystem extends SubsystemBase {
+    
+    private MotorIO topIntakeIO;
+    private MotorIO bottomIntakeIO;
+
+    public IntakeSubsystem(MotorIO topIntakeMotor, MotorIO bottomIntakeMotor)
+    {
+        this.topIntakeIO = topIntakeMotor;
+        this.bottomIntakeIO = bottomIntakeMotor;
+
+    }
+
+    @Override
+    public void periodic()
+    {
+        topIntakeIO.update();
+        bottomIntakeIO.update();
+    }
+
+}
