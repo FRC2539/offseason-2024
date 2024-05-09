d0a0aace318bc74d8a1fd9b3c24b26b05033c023
Elliptication <amajha@hershey.k12.pa.us>
2024-05-09 19:44:33 -0400

    shooterWheels sketchy stuff

--- src/main/java/frc/robot/subsystems/shooter/ShooterWheels.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterWheels.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.framework.motor.MotorIO;
@@ -9,15 +10,19 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class ShooterWheels extends SubsystemBase implements Logged{
     
+    // do not trust any numbers you see they are wrong
+
     private MotorIO topShooterIO;
     private MotorIO bottomShooterIO;
     private PIDController pid;
+    private SimpleMotorFeedforward ffwd = new SimpleMotorFeedforward(1, 2, 3); //fix values later
 
-    private DutyCycleEncoder testEncoder = new DutyCycleEncoder(2);
+    private DutyCycleEncoder testEncoder = new DutyCycleEncoder(2); //fix later
 
-    private double kP = 0.01;
-    private double kD = 0.01;
+    private double kP = 0.01; //tune later
+    private double kD = 0.01; //tune later
     private double valueToGetTo = 100;
+    
 
     public ShooterWheels(MotorIO topShooter, MotorIO bottomShooter)
     {
@@ -26,6 +31,12 @@ public class ShooterWheels extends SubsystemBase implements Logged{
         pid = new PIDController(kP, 0, kD);
 
         topShooterIO.setTargetVelocity(pid.calculate(testEncoder.get(), valueToGetTo));
+    }
+
+    @Override
+    public void periodic()
+    {
+        
     }
 
     private void setWheelVelocity(double velocity)
@@ -38,12 +49,4 @@ public class ShooterWheels extends SubsystemBase implements Logged{
     {
         return run(() -> setWheelVelocity(velocity));
     }
-
-<<<<<<< HEAD
-=======
-    
-
-
->>>>>>> 841d33851b3937e47f26949cd01bbfc84251aaa7
-
 }
